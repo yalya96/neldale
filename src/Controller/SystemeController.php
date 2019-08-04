b255dfca3647abfe33e1706950e10981c1be3d76
yalya96 <yalya96@gmail.com>
2019-08-04 04:14:28 +0000

    Ajout SADMIN ET CAISSIER

--- src/Controller/SystemeController.php
+++ src/Controller/SystemeController.php
@@ -4,6 +4,13 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use App\Entity\Prestataire;
+use App\Entity\Compte;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class SystemeController extends AbstractController
 {
@@ -15,5 +22,86 @@ class SystemeController extends AbstractController
         return $this->render('systeme/index.html.twig', [
             'controller_name' => 'SystemeController',
         ]);
+    }
+    /**
+     * @Route("/ajout")
+     */
+    public function addsys(Request $request, UserPasswordEncoderInterface $passwordEncoder, EntityManagerInterface $entityManager)
+    {
+        $values = json_decode($request->getContent());
+        if(isset($values->username) && isset($values->prenom) && isset($values->nom) && isset($values->telephone)) 
+        {
+            $a=$this->chiffre($values->prenom);
+            $b=$this->chiffre($values->nom);
+            if ((is_numeric($values->telephone) && $a==0 && $b==0) && ($values->profil==2 || $values->profil==1))
+            {
+                    $user= new User();
+                    $user->setPrenom(trim($values->prenom));
+                    $user->setNom(trim($values->nom));
+                    $user->setTelephone(trim($values->telephone));
+                    $user->setUsername(trim($values->username));
+                    $user->setPassword($passwordEncoder->encodePassword($user, "welcome"));
+                    if ($values->profil==1) {
+                        $user->setRoles(['ROLE_SUPERADMIN']);    
+                    }
+                    if ($values->profil==2) {
+                        $user->setRoles(['ROLE_CAISSIER']);    
+                    }
+                    $entityManager->persist($user);
+                    $entityManager->flush();
+                    $data=[
+                        'Message'=>'UTILISATEUR CREER',
+                        'Username'=> $user->getUsername(),
+                        'MOT DE PASSE'=>'welcome',
+                        'ROLES'=> $user->getRoles()
+                    ];
+                    return new JsonResponse($data, 500);
+            }
+            else {
+                $data = [
+                    'status' => 500,
+                    'message' => 'UTILISATEUR NONCREER1'
+                ];
+                return new JsonResponse($data, 500);
+            }
+             
+            
+                
+        }
+            else {
+                $data = [
+                    'status' => 500,
+                    'message' => 'UTILISATEUR NONCREER2'
+                ];
+                return new JsonResponse($data, 500);
+            }
+    }
+    function chiffre($test)
+    {
+        $retour=0;
+        $taille=strlen($test);
+       for ($i=0; $i < $taille; $i++) { 
+           if (is_numeric($test[$i])) {
+               $retour=1;
+                break;
+           }
+       }
+       if ($retour==0) {
+           return $retour;
+       }
+       if ($retour==1) {
+           return $retour;
+       }
+    }
+    function numerocompte()
+    {
+        $jour = date('d');
+        $mois = date('m');
+        $annee = date('Y');
+        $heure = date('H');
+        $minute = date('i');
+        $seconde = date('s');
+        $test=$seconde.$minute.$heure.$annee.$mois.$jour;
+        return $test;
     }
 }
--- var/cache/dev/UrlMatcher.php
+++ var/cache/dev/UrlMatcher.php
@@ -13,6 +13,13 @@ return [
         '/_profiler/search_bar' => [[['_route' => '_profiler_search_bar', '_controller' => 'web_profiler.controller.profiler::searchBarAction'], null, null, null, false, false, null]],
         '/_profiler/phpinfo' => [[['_route' => '_profiler_phpinfo', '_controller' => 'web_profiler.controller.profiler::phpinfoAction'], null, null, null, false, false, null]],
         '/_profiler/open' => [[['_route' => '_profiler_open_file', '_controller' => 'web_profiler.controller.profiler::openAction'], null, null, null, false, false, null]],
+        '/authentification' => [[['_route' => 'authentification', '_controller' => 'App\\Controller\\AuthentificationController::index'], null, null, null, false, false, null]],
+        '/prestataire' => [
+            [['_route' => 'prestataire', '_controller' => 'App\\Controller\\PrestataireController::index'], null, null, null, false, false, null],
+            [['_route' => 'app_systeme_addprest', '_controller' => 'App\\Controller\\SystemeController::addprest'], null, null, null, false, false, null],
+        ],
+        '/systeme' => [[['_route' => 'systeme', '_controller' => 'App\\Controller\\SystemeController::index'], null, null, null, false, false, null]],
+        '/ajout' => [[['_route' => 'app_systeme_addsys', '_controller' => 'App\\Controller\\SystemeController::addsys'], null, null, null, false, false, null]],
     ],
     [ // $regexpList
         0 => '{^(?'
@@ -34,8 +41,36 @@ return [
                 .'|/api(?'
                     .'|(?:/(index)(?:\\.([^/]++))?)?(*:204)'
                     .'|/(?'
-                        .'|docs(?:\\.([^/]++))?(*:235)'
-                        .'|contexts/(.+)(?:\\.([^/]++))?(*:271)'
+                        .'|d(?'
+                            .'|ocs(?:\\.([^/]++))?(*:238)'
+                            .'|epots(?'
+                                .'|(?:\\.([^/]++))?(?'
+                                    .'|(*:272)'
+                                .')'
+                                .'|/([^/\\.]++)(?:\\.([^/]++))?(?'
+                                    .'|(*:310)'
+                                .')'
+                            .')'
+                        .')'
+                        .'|co(?'
+                            .'|ntexts/(.+)(?:\\.([^/]++))?(*:352)'
+                            .'|mptes(?'
+                                .'|(?:\\.([^/]++))?(?'
+                                    .'|(*:386)'
+                                .')'
+                                .'|/([^/\\.]++)(?:\\.([^/]++))?(?'
+                                    .'|(*:424)'
+                                .')'
+                            .')'
+                        .')'
+                        .'|prestataires(?'
+                            .'|(?:\\.([^/]++))?(?'
+                                .'|(*:468)'
+                            .')'
+                            .'|/([^/\\.]++)(?:\\.([^/]++))?(?'
+                                .'|(*:506)'
+                            .')'
+                        .')'
                     .')'
                 .')'
             .')/?$}sDu',
@@ -49,9 +84,34 @@ return [
         149 => [[['_route' => '_profiler_exception_css', '_controller' => 'web_profiler.controller.exception::cssAction'], ['token'], null, null, false, false, null]],
         159 => [[['_route' => '_profiler', '_controller' => 'web_profiler.controller.profiler::panelAction'], ['token'], null, null, false, true, null]],
         204 => [[['_route' => 'api_entrypoint', '_controller' => 'api_platform.action.entrypoint', '_format' => '', '_api_respond' => 'true', 'index' => 'index'], ['index', '_format'], null, null, false, true, null]],
-        235 => [[['_route' => 'api_doc', '_controller' => 'api_platform.action.documentation', '_format' => '', '_api_respond' => 'true'], ['_format'], null, null, false, true, null]],
-        271 => [
-            [['_route' => 'api_jsonld_context', '_controller' => 'api_platform.jsonld.action.context', '_format' => 'jsonld', '_api_respond' => 'true'], ['shortName', '_format'], null, null, false, true, null],
+        238 => [[['_route' => 'api_doc', '_controller' => 'api_platform.action.documentation', '_format' => '', '_api_respond' => 'true'], ['_format'], null, null, false, true, null]],
+        272 => [
+            [['_route' => 'api_depots_get_collection', '_controller' => 'api_platform.action.get_collection', '_format' => null, '_api_resource_class' => 'App\\Entity\\Depot', '_api_collection_operation_name' => 'get'], ['_format'], ['GET' => 0], null, false, true, null],
+            [['_route' => 'api_depots_post_collection', '_controller' => 'api_platform.action.post_collection', '_format' => null, '_api_resource_class' => 'App\\Entity\\Depot', '_api_collection_operation_name' => 'post'], ['_format'], ['POST' => 0], null, false, true, null],
+        ],
+        310 => [
+            [['_route' => 'api_depots_get_item', '_controller' => 'api_platform.action.get_item', '_format' => null, '_api_resource_class' => 'App\\Entity\\Depot', '_api_item_operation_name' => 'get'], ['id', '_format'], ['GET' => 0], null, false, true, null],
+            [['_route' => 'api_depots_delete_item', '_controller' => 'api_platform.action.delete_item', '_format' => null, '_api_resource_class' => 'App\\Entity\\Depot', '_api_item_operation_name' => 'delete'], ['id', '_format'], ['DELETE' => 0], null, false, true, null],
+            [['_route' => 'api_depots_put_item', '_controller' => 'api_platform.action.put_item', '_format' => null, '_api_resource_class' => 'App\\Entity\\Depot', '_api_item_operation_name' => 'put'], ['id', '_format'], ['PUT' => 0], null, false, true, null],
+        ],
+        352 => [[['_route' => 'api_jsonld_context', '_controller' => 'api_platform.jsonld.action.context', '_format' => 'jsonld', '_api_respond' => 'true'], ['shortName', '_format'], null, null, false, true, null]],
+        386 => [
+            [['_route' => 'api_comptes_get_collection', '_controller' => 'api_platform.action.get_collection', '_format' => null, '_api_resource_class' => 'App\\Entity\\Compte', '_api_collection_operation_name' => 'get'], ['_format'], ['GET' => 0], null, false, true, null],
+            [['_route' => 'api_comptes_post_collection', '_controller' => 'api_platform.action.post_collection', '_format' => null, '_api_resource_class' => 'App\\Entity\\Compte', '_api_collection_operation_name' => 'post'], ['_format'], ['POST' => 0], null, false, true, null],
+        ],
+        424 => [
+            [['_route' => 'api_comptes_get_item', '_controller' => 'api_platform.action.get_item', '_format' => null, '_api_resource_class' => 'App\\Entity\\Compte', '_api_item_operation_name' => 'get'], ['id', '_format'], ['GET' => 0], null, false, true, null],
+            [['_route' => 'api_comptes_delete_item', '_controller' => 'api_platform.action.delete_item', '_format' => null, '_api_resource_class' => 'App\\Entity\\Compte', '_api_item_operation_name' => 'delete'], ['id', '_format'], ['DELETE' => 0], null, false, true, null],
+            [['_route' => 'api_comptes_put_item', '_controller' => 'api_platform.action.put_item', '_format' => null, '_api_resource_class' => 'App\\Entity\\Compte', '_api_item_operation_name' => 'put'], ['id', '_format'], ['PUT' => 0], null, false, true, null],
+        ],
+        468 => [
+            [['_route' => 'api_prestataires_get_collection', '_controller' => 'api_platform.action.get_collection', '_format' => null, '_api_resource_class' => 'App\\Entity\\Prestataire', '_api_collection_operation_name' => 'get'], ['_format'], ['GET' => 0], null, false, true, null],
+            [['_route' => 'api_prestataires_post_collection', '_controller' => 'api_platform.action.post_collection', '_format' => null, '_api_resource_class' => 'App\\Entity\\Prestataire', '_api_collection_operation_name' => 'post'], ['_format'], ['POST' => 0], null, false, true, null],
+        ],
+        506 => [
+            [['_route' => 'api_prestataires_get_item', '_controller' => 'api_platform.action.get_item', '_format' => null, '_api_resource_class' => 'App\\Entity\\Prestataire', '_api_item_operation_name' => 'get'], ['id', '_format'], ['GET' => 0], null, false, true, null],
+            [['_route' => 'api_prestataires_delete_item', '_controller' => 'api_platform.action.delete_item', '_format' => null, '_api_resource_class' => 'App\\Entity\\Prestataire', '_api_item_operation_name' => 'delete'], ['id', '_format'], ['DELETE' => 0], null, false, true, null],
+            [['_route' => 'api_prestataires_put_item', '_controller' => 'api_platform.action.put_item', '_format' => null, '_api_resource_class' => 'App\\Entity\\Prestataire', '_api_item_operation_name' => 'put'], ['id', '_format'], ['PUT' => 0], null, false, true, null],
             [null, null, null, null, false, false, 0],
         ],
     ],
